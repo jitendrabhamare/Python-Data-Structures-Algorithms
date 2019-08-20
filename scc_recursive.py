d93e497486f0ecf4378631587832b443a6dd1055
Jitendra Bhamare <jitendra0905@gmail.com>
2019-08-19 22:54:45 -0700

    Added kosaraju algo code.

---
+++ scc_recursive.py
@@ -0,0 +1,89 @@
+## Kosaraju's Algorithm to find strongly connected components (SCC)
+
+import sys, threading
+sys.setrecursionlimit(800000)
+threading.stack_size(67108864)
+
+def main():
+    # Load an input text file and generate graph and reversed graph from it. 
+    #input_file = open("scc-small.txt", "r")
+    input_file = open("SCC.txt", "r")
+    data = input_file.readlines()
+    # Declare num of nodes from input files. always define with one extra node
+    num_nodes = 875715  
+
+    graph = [[] for i in range(num_nodes)]
+    rgraph = [[] for i in range(num_nodes)]
+
+    for item in data:
+        item = item.split()
+        graph[int(item[0])] += [int(item[1])]
+        rgraph[int(item[1])] += [int(item[0])]
+
+    #print("graph: {}".format(graph))
+    #print("rev-graph: {}".format(rgraph))
+
+    ## Intialize lists for pass1
+    explored = [False] * num_nodes
+    leader = [0] * num_nodes
+    #order = [0] * num_nodes
+    order = []
+
+    ## DFS_Loop function
+    def DFS_Loop(graph, pass_list):
+        global t # Num of nodes processed so far
+        t = 0
+        global s # Current source vertex
+        s = None
+
+        for i in pass_list:
+            if explored[i] == False:
+                s = i # assign to start node
+                DFS(graph, i)
+
+    ## Recursive DFS Function
+    def DFS(graph, i):
+        explored[i] = True
+
+        for neighbor in graph[i]:
+            if explored[neighbor] == False:
+                DFS(graph, neighbor)
+        global t
+        t += 1
+        # order list is used in second pass 
+        order.append(i) # append and reverse list works much faster than prepend
+        global s
+        leader[s] += 1
+
+    ## Run DFS_Loop on rev graph
+    pass1_list = range(num_nodes-1, 0, -1)
+    DFS_Loop(rgraph, pass1_list)
+
+    #print("finishing order: {}".format(order))
+    #print("leader : {}".format(leader))
+    #print("explored : {}".format(explored))
+
+
+    ## Intialize lists for pass2
+    order.reverse()
+    pass2_list = order
+    #print(pass2_list)
+    new_size = len(order)+1 # Will not consider disconnected graphs
+    order = [0] * new_size 
+    explored = [False] * new_size
+    leader = [0] * new_size
+
+    ## Run DFS_Loop on graph
+    DFS_Loop(graph, pass2_list)
+
+    #print("finishing order: {}".format(order))
+    #print("leader : {}".format(leader))
+    #print("explored : {}".format(explored))
+
+    #getting 5 biggest scc
+    leader.sort(reverse=True)
+    print("First largest 5 SCCs: {}".format(leader[:5]))
+
+
+thread = threading.Thread(target=main)
+thread.start()    
